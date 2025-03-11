907ecca49a4c3ac4f5e4c7b92978efddaffbd2b2
yuexiwei <gavin.yxw@gmail.com>
2025-03-11 16:39:37 +0800

    add Web Analytics

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 访问统计
 async function loadStats() {
     try {
-        const response = await fetch('https://cloud.umami.is/share/8wyuz6s73sMeOkDY/newcoder.life');
+        const response = await fetch('https://cloud.umami.is/share/newcoder.life');
         const data = await response.json();
         document.getElementById('counter').textContent = `PV: ${data.pageviews.value}, UV: ${data.visitors.value}`;
     } catch (error) {
